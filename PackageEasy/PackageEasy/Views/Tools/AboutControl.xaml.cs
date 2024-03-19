428cdcf9fba3e654110efe5db362047fc29bfdeb
tangmanger <tangmanger@outlook.com>
2024-03-19 23:54:13 +0800

    fix 关于界面

--- PackageEasy/PackageEasy/Views/Tools/AboutControl.xaml.cs
+++ PackageEasy/PackageEasy/Views/Tools/AboutControl.xaml.cs
@@ -1,7 +1,10 @@
-﻿using PackageEasy.Common.Data;
+﻿using CommunityToolkit.Mvvm.Input;
+using PackageEasy.Common.Data;
 using PackageEasy.Controls.Controls;
+using PackageEasy.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Windows.Forms.LinkLabel;
 
 namespace PackageEasy.Views.Tools
 {
@@ -22,9 +26,13 @@ namespace PackageEasy.Views.Tools
     /// </summary>
     public partial class AboutControl : BaseControl
     {
+        private string version;
+
         public AboutControl()
         {
             InitializeComponent();
+            DataContext = this;
+            Version = CacheDataHelper.Version;
         }
 
         public override string Description => "关于PackageEasy".GetLangText();
@@ -41,5 +49,37 @@ namespace PackageEasy.Views.Tools
         public override void Unload()
         {
         }
+
+        #region 属性
+
+        /// <summary>
+        /// 版本
+        /// </summary>
+        public string Version
+        {
+            get => version;
+            set
+            {
+                version = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region 命令 
+
+
+        /// <summary>
+        /// 导航
+        /// </summary>
+        public RelayCommand<Uri> NavigateCommand => new RelayCommand<Uri>((uri) =>
+        {
+            if (uri == null) return;
+            Process.Start(new ProcessStartInfo(uri.AbsoluteUri) { UseShellExecute = true });
+            //Process.Start(uri.OriginalString);
+        });
+
+        #endregion
     }
 }
